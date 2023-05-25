1098545706f058bac16fafb1ec8d6fd43759c74b
Guilherme Fernando de Souza <guilherme_h14@hotmail.com>
2023-05-25 20:33:00 -0300

    validaçoes

--- EtecBookUI/src/app/components/register/register.component.ts
+++ EtecBookUI/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm!: FormGroup;
+
+  constructor (private fb: FormBuilder) {}
+    
+    ngOnInit(): void {
+      this.registerForm = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', Validators.compose([Validators.required, Validators.email])],
+        password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      });
+    }
+
+    checkName(){
+      return this.registerForm.controls['name'].dirty && this.registerForm.controls['name'].errors?.['required'];
+    }
+    
+    checkEmail(){
+      return this.registerForm.controls['email'].dirty && this.registerForm.controls['email'].errors?.['required'];
+    }
+
+    checkEmailValid(){
+      return this.registerForm.controls['email'].dirty && this.registerForm.controls['email'].errors?.['email'];
+    }
+
+    checkPassword() {
+      return this.f['password'].dirty && this.f['password'].errors?.['minLength'];
+    }
+
 }
+
+
+
